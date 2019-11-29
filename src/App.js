27de9b968ca59986613d21864caaf0022323ca52
Avinash <cloud.avi81@gmail.com>
2019-11-29 12:32:38 +0800

    Updated feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning Docker with Kubernetes
+          Learning Docker with Kubernetes in feature branch
         </p>
         <a
           className="App-link"
